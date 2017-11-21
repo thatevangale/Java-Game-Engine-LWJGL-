6dfcef35a88e946f552f5490b2513c39766cab6d
Evan Gale <thatevangale@gmail.com>
2017-11-22 10:18:57 +1100

    Creates a blank window

--- src/evan/game/main/GameMain.java
+++ src/evan/game/main/GameMain.java
@@ -1,100 +1,81 @@
 package evan.game.main;
 
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+import java.nio.ByteBuffer;
+import org.lwjgl.glfw.GLFWVidMode;
+
 public class GameMain {
 	
-	private static final long serialVersionUID = 1L;
+	private static final int WIDTH = 640;
+	private static final int HEIGHT = 480;
 	
-	public static final int TARGET_FPS = 60;
-	public static final int TARGET_UPS = 30;
+	private boolean running = false;
 	
-	public static final int WIDTH = 640;
-	public static final int HEIGHT = 480;
+	private long windowID;
 	
-	protected boolean running;
-	
-	protected GameWindow window;
-	
-	protected GameTimer timer;
-	
-	protected GameRenderer renderer;
-	
-	public GameMain() {
-		timer = new GameTimer();
-		renderer = new GameRenderer();
-	}
-
 	public static void main(String[] args) {
 		GameMain game = new GameMain();
-		game.start();
+		game.run();
 	}
-
-	public void start() {
+	
+	public void run() {
 		init();
-		gameLoop();
+		
+		while (running) {
+			//input();
+			update();
+			render();
+			
+			if (glfwWindowShouldClose(windowID)) {
+				running = false;
+			}
+		}
+		
 		dispose();
-	}
-
-	private void init() {
-		timer.init();
-		running = true;
-	}
-
-	public void gameLoop() {
-		double delta;
-        double accumulator = 0.0;
-        double interval = 1.0 / TARGET_UPS;
-        double alpha;
-
-        while (running) {
-            /* Check if game should close */
-            if (window.isClosing()) {
-                running = false;
-            }
-
-            /* Get delta time and update the accumulator */
-            delta = timer.getDelta();
-            accumulator += delta;
-
-            /* Handle input */
-            input();
-
-            /* Update game and timer UPS if enough time has passed */
-            while (accumulator >= interval) {
-                update(interval);
-                //timer.updateUPS();
-                accumulator -= interval;
-            }
-
-            /* Calculate alpha value for interpolation */
-            alpha = accumulator / interval;
-
-            /* Render game and update timer FPS */
-            render(alpha);
-            //timer.updateFPS();
-
-            /* Update timer */
-            //timer.update();
-        }
-    }
-	
-	private void input() {
-		// TODO Auto-generated method stub
 		
+		glfwDestroyWindow(windowID);
+		glfwTerminate();
+		System.exit(0);
+	}
+
+	public void init() {
+		this.running = true;
+		
+		if (!glfwInit()) {
+			System.err.println("Error: GLFW initialization failed");
+			System.exit(-1);
+		}
+		
+		glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+		
+		windowID = glfwCreateWindow(WIDTH, HEIGHT, "Pong", NULL, NULL);
+		
+		if (windowID == NULL) {
+			System.err.println("Error: Game window code could not be created");
+		}
+		
+		GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+		
+		glfwSetWindowPos(windowID, 100, 100);
+		
+		glfwMakeContextCurrent(windowID);
+		
+		glfwShowWindow(windowID);
 	}
 	
-	private void update(double interval) {
-		// TODO Auto-generated method stub
-		
+	public void input() {}
+	
+	public void update() {
+		glfwPollEvents();
 	}
 	
-	private void render(double alpha) {
-		// TODO Auto-generated method stub
-		
+	public void render() {
+		glfwSwapBuffers(windowID);
 	}
-
-	private static void dispose() {
-		// TODO Auto-generated method stub
-		
-	}
-
+	
+	private void dispose() {}
+	
 }
--- src/evan/game/main/GameRenderer.java
+++
@@ -1,5 +0,0 @@
-package evan.game.main;
-
-public class GameRenderer {
-
-}
